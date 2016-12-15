dd883fb7bad044af3ccea2e9cea1a6f510d118b3
Quentin S <telitos@gmail.com>
2016-12-15 14:19:11 -0500

    adds test fetch

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+   fetch('http://www.localhost:8080/')
+    .then((response) => {
+      return response.text();
+    })
+    .then((body) => {
+      console.log(body)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2> to React </h2>
+          <h2> Welcome, to the react world... </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
