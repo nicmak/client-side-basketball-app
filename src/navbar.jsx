747a62498e540a47b05d07d34c150e76523f940e
nicmak <nicholas123mak@gmail.com>
2016-12-16 14:08:45 +0000

    Added nav bar and added team cards

---
+++ src/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {AppBar} from 'material-ui';
+class Navbar extends Component {
+
+  render () {
+    return (
+    <div>
+      <AppBar
+        title="Basketball Cards"
+      />
+    </div>
+   )}
+}
+export default Navbar;
