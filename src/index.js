0a67a8df2c4d606c9d5eb574dc71a5c89ee1c9c0
nicmak <nicholas123mak@gmail.com>
2017-01-03 04:31:57 +0000

    Video and Glacier

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import {Router, Route, browserHistory} from 'react-router';
 import App from './App.jsx';
 import InventoryPage from './InventoryPage.jsx'
 import '../styles/index.css';
+import ReactDriveIn from 'react-drive-in';
 
 const root = document.getElementById('root')
 ReactDOM.render(
+	<div>
+	<ReactDriveIn 
+    show="http://raw.githubusercontent.com/ronik-design/react-drive-in/master/example/glacier.mp4"
+	/>
   <Router history={browserHistory}>
     <Route path="/" component={App}/>
     {/* <Route path="/InventoryPage/:id" component={InventoryPage}/> */}
   </Router>
+  </div>
   ,root);
